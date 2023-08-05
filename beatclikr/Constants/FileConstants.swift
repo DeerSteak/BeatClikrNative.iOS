74bd1b57d5e827c892373e3c4ef14cc99e6caf5a
Ben Funk <bfunk1978@icloud.com>
2023-08-05 14:59:11 -0500

    Add some constants

---
+++ beatclikr/Constants/FileConstants.swift
@@ -0,0 +1,28 @@
+//
+//  FileConstants.swift
+//  beatclikr
+//
+//  Created by Ben Funk on 8/5/23.
+//
+
+import Foundation
+struct FileConstants {
+    static let FileExt: STring = "wav"
+    
+    static let ClickHi: String = "clickhi"
+    static let ClickLo: String = "clicklo"
+    static let Cowbell: String = "cowbell"
+    static let CrashL: String = "crashl"
+    static let CrashR: String = "crashr"
+    static let HatClosed: String = "hatclosed"
+    static let HatOpen: String = "hatopen"
+    static let Kick: String = "kick"
+    static let RideEdge: String = "ride"
+    static let RideBell: String = "ridebell"
+    static let Silence: String = "silence"
+    static let Snare: String = "snare"
+    static let Tamb: String = "tamb"
+    static let TomHi: String = "tomhi"
+    static let TomLo: String = "tomlow"
+    static let TomMid: String = "tommid"
+}
---
+++ beatclikr/Constants/PreferenceKeys.swift
@@ -0,0 +1,28 @@
+//
+//  PreferenceKeys.swift
+//  beatclikr
+//
+//  Created by Ben Funk on 8/5/23.
+//
+
+import Foundation
+
+struct PreferenceKeys {
+    static let InstantBeat = "InstantBeat"
+    static let InstantRhythm = "InstantRhythm"
+    static let RehearsalBeat = "RehearsalBeat"
+    static let RehearsalRhythm = "RehearsalRhythm"
+    static let LiveBeat = "LiveBeat"
+    static let LiveRhythm = "LiveRhythm"
+    static let MuteMetronome = "MuteMetronome"
+    static let UseFlashlight = "UseFlashlight"
+    static let AskFlashlight = "AskFlashlight"
+    static let Onboarded = "Onboarded"
+    static let UseHaptic = "UseHaptic"
+    static let InstantSelectedSubdivisionIndex = "InstantSelectedSubdivisionIndex"
+    static let InstantBpm = "InstantBpm"
+    static let HasAskedFlashlight = "HasAskedFlashlight"
+    static let HasAskedHaptic = "HasAskedHaptic"
+    static let PracticeReminders = "SendPracticeReminders"
+    static let HasAskedReminders = "HasAskedReminders"
+}
