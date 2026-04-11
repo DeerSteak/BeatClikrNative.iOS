8ad5eaff7fd6bd7d2a66ddc3f9bc1cc9502ac436
Ben Funk <bfunk1978@icloud.com>
2026-04-10 22:30:33 -0500

    resolve extra line in slider

--- beatclikr/View/InstantMetronomeView.swift
+++ beatclikr/View/InstantMetronomeView.swift
@@ -19,87 +19,87 @@ struct InstantMetronomeView: View {
     
     var body: some View {
         NavigationStack {
-        Grid(alignment: .trailing, verticalSpacing: 16) {
-            HStack{
-                Spacer()
-                ZStack {
-                    // Invisible spacer to maintain fixed height at 100% scale
-                    Color.clear
-                        .frame(width: MetronomeConstants.playerViewDefaultSize, height: MetronomeConstants.playerViewDefaultSize)
-
-                    MetronomePlayerView()
+            VStack(spacing: 20) {
+                HStack{
+                    Spacer()
+                    ZStack {
+                        // Invisible spacer to maintain fixed height at 100% scale
+                        Color.clear
+                            .frame(width: MetronomeConstants.playerViewDefaultSize, height: MetronomeConstants.playerViewDefaultSize)
+                        
+                        MetronomePlayerView()
+                    }
+                    .padding(.all, 12)
+                    Spacer()
                 }
-                .padding(.all, 12)
-                Spacer()
-            }
-            VStack {
-                Text("Tempo (BPM): \(FormatterHelper.formatDouble(model.beatsPerMinute))")
-                Slider(value: $model.beatsPerMinute, in: MetronomeConstants.defaultMinSliderBPM...MetronomeConstants.defaultMaxSliderBPM, step: 1) {
-                    Text("Tempo")
-                } minimumValueLabel: {
-                    Text("\(Int(MetronomeConstants.defaultMinSliderBPM))")
-                } maximumValueLabel: {
-                    Text("\(Int(MetronomeConstants.defaultMaxSliderBPM))")
-                }
-                .onChange(of: model.beatsPerMinute) {
-                    resetMetronome()
-                }
-            }
-            GridRow {
-                Text("Subdivisions")
-                Picker("Select Groove", selection: $model.selectedGroove) {
-                    ForEach(Groove.allCases) {
-                        option in
-                        Text(String(describing: option))
+                VStack {
+                    Text("Tempo (BPM): \(FormatterHelper.formatDouble(model.beatsPerMinute))")
+                    HStack {
+                        Text("\(Int(MetronomeConstants.defaultMinSliderBPM))")
+                        Slider(value: $model.beatsPerMinute, in: MetronomeConstants.defaultMinSliderBPM...MetronomeConstants.defaultMaxSliderBPM, step: 1)
+                        Text("\(Int(MetronomeConstants.defaultMaxSliderBPM))")
+                    }
+                    .onChange(of: model.beatsPerMinute) {
+                        resetMetronome()
                     }
                 }
-                .onChange(of: model.selectedGroove) { resetMetronome() }
-                .pickerStyle(.segmented)
-            }
-            GridRow {
-                Text("Beat")
-                Menu(content: {
-                    Picker("Beat", selection: $model.beat) {
-                        ForEach(InstrumentLists.beat) {
-                            option in
-                            Text(String(describing: option))
+                Grid(alignment: .trailing, verticalSpacing: 16) {
+                    GridRow {
+                        Text("Subdivisions")
+                        Picker("Select Groove", selection: $model.selectedGroove) {
+                            ForEach(Groove.allCases) {
+                                option in
+                                Text(String(describing: option))
+                            }
                         }
+                        .onChange(of: model.selectedGroove) { resetMetronome() }
+                        .pickerStyle(.segmented)
                     }
-                    .onChange(of: model.beat) { resetMetronome() }
-                    .pickerStyle(.inline)
-                    .labelsHidden()
-                }, label: {
-                    RectangleText("\(model.beat.description)", backgroundColor: Color(UIColor.systemBackground), foregroundColor: .appPrimary)
-                })
-            }
-            GridRow {
-                Text("Rhythm")
-                Menu(content: {
-                    Picker("Rhythm", selection: $model.rhythm) {
-                        ForEach(InstrumentLists.rhythm) {
-                            option in
-                            Text(String(describing: option))
-                        }
+                    GridRow {
+                        Text("Beat")
+                        Menu(content: {
+                            Picker("Beat", selection: $model.beat) {
+                                ForEach(InstrumentLists.beat) {
+                                    option in
+                                    Text(String(describing: option))
+                                }
+                            }
+                            .onChange(of: model.beat) { resetMetronome() }
+                            .pickerStyle(.inline)
+                            .labelsHidden()
+                        }, label: {
+                            RectangleText("\(model.beat.description)", backgroundColor: Color(UIColor.systemBackground), foregroundColor: .appPrimary)
+                        })
                     }
-                    .onChange(of: model.rhythm) { resetMetronome() }
-                    .pickerStyle(.inline)
-                    .labelsHidden()
-                }, label: {
-                    RectangleText("\(model.rhythm.description)", backgroundColor: Color(UIColor.systemBackground), foregroundColor: .appPrimary)
+                    GridRow {
+                        Text("Rhythm")
+                        Menu(content: {
+                            Picker("Rhythm", selection: $model.rhythm) {
+                                ForEach(InstrumentLists.rhythm) {
+                                    option in
+                                    Text(String(describing: option))
+                                }
+                            }
+                            .onChange(of: model.rhythm) { resetMetronome() }
+                            .pickerStyle(.inline)
+                            .labelsHidden()
+                        }, label: {
+                            RectangleText("\(model.rhythm.description)", backgroundColor: Color(UIColor.systemBackground), foregroundColor: .appPrimary)
+                        })
+                        
+                    }
+                }
+                Button(action: togglePlayPause, label: {
+                    RectangleText(model.isPlaying ? String(localized: "Pause") : String(localized: "Play"))
                 })
                 
+                Spacer()
+                
             }
-            Button(action: togglePlayPause, label: {
-                RectangleText(model.isPlaying ? String(localized: "Pause") : String(localized: "Play"))
-            })
-            
-            Spacer()
-            
-        }
-        .onDisappear(perform: model.stop)
-        .onAppear(perform: { model.clickerType = .instant })
-        .padding(.all, 12)
-        .navigationTitle("Instant Metronome")
+            .onDisappear(perform: model.stop)
+            .onAppear(perform: { model.clickerType = .instant })
+            .padding(.all, 12)
+            .navigationTitle("Instant Metronome")
         }
     }
     
@@ -125,5 +125,5 @@ struct InstantMetronomeView: View {
     return InstantMetronomeView()
         .modelContainer(previewContainer.container)
         .environmentObject(MetronomePlaybackViewModel())
-        
+    
 }
