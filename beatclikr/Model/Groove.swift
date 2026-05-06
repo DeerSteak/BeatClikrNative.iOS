4023f9f1092cc1d1c043f17772df004754166f96
Ben Funk <bfunk1978@icloud.com>
2026-05-05 20:27:46 -0500

    Update models

--- beatclikr/Model/Groove.swift
+++ beatclikr/Model/Groove.swift
@@ -11,18 +11,30 @@ enum Groove: Int, CaseIterable, Identifiable, CustomStringConvertible, Codable {
     case eighth = 2
     case triplet = 3
     case sixteenth = 4
-    
-    var id: Self {self}
+    case oddMeterQuarter = 5
+    case oddMeterEighth = 6
+
+    var id: Self { self }
     var description: String {
         switch self {
-        case .quarter:
-            return "Quarter Note"
-        case .eighth:
-            return "Eighth Note"
-        case .triplet:
-            return "Triplet 6/8"
-        case .sixteenth:
-            return "Sixteenth Note"
+        case .quarter:         return "Quarter Note"
+        case .eighth:          return "Eighth Note"
+        case .triplet:         return "Triplet 6/8"
+        case .sixteenth:       return "Sixteenth Note"
+        case .oddMeterQuarter: return "Odd Quarter"
+        case .oddMeterEighth:  return "Odd Eighth"
+        }
+    }
+
+    var isOddMeter: Bool {
+        self == .oddMeterQuarter || self == .oddMeterEighth
+    }
+
+    var subdivisions: Int {
+        switch self {
+        case .oddMeterEighth: return 2
+        case .oddMeterQuarter: return 1
+        default: return rawValue
         }
     }
 }
