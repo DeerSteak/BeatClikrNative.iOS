6d03bc83f8a0ab00d3ebd6aa76f015fb60722cab
Ben Funk <bfunk1978@icloud.com>
2026-05-16 07:42:18 -0500

    fix vibration delay

--- beatclikr/Services/VibrationService.swift
+++ beatclikr/Services/VibrationService.swift
@@ -15,11 +15,18 @@ class VibrationService {
     private var beatGenerator = UIImpactFeedbackGenerator(style: .heavy)
     private var rhythmGenerator = UIImpactFeedbackGenerator(style: .soft)
 
+    func prepare() {
+        beatGenerator.prepare()
+        rhythmGenerator.prepare()
+    }
+
     func vibrateBeat() {
         beatGenerator.impactOccurred()
+        beatGenerator.prepare()
     }
 
     func vibrateRhythm() {
         rhythmGenerator.impactOccurred()
+        rhythmGenerator.prepare()
     }
 }
